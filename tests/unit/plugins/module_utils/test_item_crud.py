e92d6009d1bddd545b60088fb4bd1ca062927ee0
david.gunter <david.gunter@agilebits.com>
2021-05-10 17:22:38 -0400

    Update tests to use new present/absent state values. Update the unit tests to support the new helpers.
    A follow-up PR in the future should introduce a model class for better readability.

--- tests/unit/plugins/module_utils/test_item_crud.py
+++ tests/unit/plugins/module_utils/test_item_crud.py
@@ -11,12 +11,15 @@ def test_create_item(mocker):
     params = {
         "vault_id": "Abc123",
         "category": const.ItemType.PASSWORD,
-        "title": "My New Item",
+        "name": "My New Item",
         "favorite": True
     }
 
     mock_api = mocker.Mock()
-    mock_api.create_item.return_value = {}
+    mock_api.create_item.return_value = {
+        "id": "abc123xyz9325",
+        **params
+    }
 
     modified, new_item = vault.create_item(params, mock_api)
 
@@ -24,56 +27,102 @@ def test_create_item(mocker):
     mock_api.create_item.assert_called_once()
 
 
-def test_delete_item(mocker):
-    item_id = "abc123"
-    params = {
-        "vault_id": "Abc123",
-        "category": const.ItemType.PASSWORD,
-        "title": "My New Item",
-        "favorite": True
-    }
+def test_check_mode(mocker):
+    vault_id = "Abc123"
 
     mock_api = mocker.Mock()
-    mock_api.get_item_by_name.return_value = {"id": item_id, "title": params["title"]}
+    create_item_params = {
+        "vault_id": vault_id,
+        "category": const.ItemType.PASSWORD,
+        "name": "My New Item",
+        "favorite": True
+    }
+    modified, item = vault.create_item(create_item_params, mock_api, check_mode=True)
+    assert modified
+    assert mock_api.create_item.mock_calls == []
 
-    modified, new_item = vault.delete_item(params, mock_api)
+    item["vault"] = {"id": create_item_params["vault_id"]}
+    item["id"] = "987654321"
+
+    update_params = {
+        **{k: v for k, v in create_item_params.items() if k != "vault_id"},
+        "vault_id": vault_id,
+        "name": "UPDATED Title",
+    }
+
+    modified, updated_item = vault.update_item(update_params, item, mock_api, check_mode=True)
+    assert modified
+    assert mock_api.update_item.mock_calls == []
+    assert updated_item["title"] == update_params["name"]
+
+    modified, delete_response = vault.delete_item(updated_item, mock_api, check_mode=True)
+    assert modified
+    assert delete_response == {}
+    assert mock_api.delete_item.mock_calls == []
+
+
+def test_delete_item(mocker):
+    item = {
+        "id": "xyz9876",
+        "vault": {"id": "ABC123"},
+        "category": const.ItemType.PASSWORD,
+        "title": "My New Item"
+    }
+    mock_api = mocker.Mock()
+
+    modified, new_item = vault.delete_item(item, mock_api)
 
     assert modified is True
-    mock_api.get_item_by_name.assert_called_once_with(params["vault_id"], item_name=params["title"])
-    mock_api.delete_item.assert_called_once_with(params["vault_id"], item_id=item_id)
+    mock_api.delete_item.assert_called_once_with(item["vault"]["id"], item_id=item["id"])
 
 
 def test_delete_item_when_does_not_exist(mocker):
-    params = {
-        "vault_id": "Abc123",
-        "category": const.ItemType.PASSWORD,
-        "title": "My New Item",
-        "favorite": True
-    }
-
     mock_api = mocker.Mock()
-    mock_api.get_item_by_name.side_effect = errors.NotFoundError
 
-    modified, resp = vault.delete_item(params, mock_api)
+    non_existent_item = None
+    modified, resp = vault.delete_item(non_existent_item, mock_api)
 
     assert modified is False
     assert not mock_api.delete_item.called
 
 
 def test_update_item_create_when_not_exists(mocker):
-    params = {
-        "uuid": "XYZ123def456",
-        "vault_id": "Abc123",
+    vault_id = "Abc123"
+    original_item = {
+        "id": "XYZ123def456",
+        "vault": {"id": vault_id},
+        "title": "BEFORE_NAME",
         "category": const.ItemType.PASSWORD,
-        "title": "My New Item",
         "favorite": True
     }
 
-    mock_api = mocker.Mock()
-    mock_api.get_item_by_id.side_effect = errors.NotFoundError
-    mock_api.create_item.return_value = {"id": params["uuid"], "vault": {"id": params["vault_id"]}}
+    params = {
+        "favorite": False,
+        "title": "AFTER_NAME",
+        "vault_id": vault_id,
+        "category": const.ItemType.PASSWORD,
+        "value": "hunter2",
+        "fields": [
+            {
+                "label": "Password",
+                "value": "hunter2",
+                "field_type": const.FieldType.CONCEALED
+            }
+        ]
+    }
 
-    modified, updated_item = vault.upsert_item(params, mock_api)
+    mock_api = mocker.Mock()
+    mock_api.update_item.return_value = {
+        "id": original_item["id"],
+        "vault": original_item["vault"],
+        "title": params["title"],
+        "fields": params["fields"],
+        "category": params["category"],
+        "favorite": params["favorite"]
+    }
+
+    modified, updated_item = vault.update_item(params, original_item, mock_api)
 
     assert modified is True
-    assert updated_item
+    assert updated_item["favorite"] == params["favorite"]
+    assert updated_item["title"] == params["title"]
