b03f5d858e3ddca8c61bf98c396cbcf8b3fa2bcb
Volodymyr Zotov <volodymyr.zotov@agilebits.com>
2023-09-19 14:32:06 -0500

    Update LICENSE.md to use GPL instead of MIT
    This is a requirement for ansible collection to to with GPL https://docs.ansible.com/ansible/latest/community/collection_contributors/collection_requirements.html#collection-licensing-requirements

--- plugins/modules/item_info.py
+++ plugins/modules/item_info.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-#  Copyright (C) 2021  1Password & Agilebits (@1Password)
+#  Copyright (C) 2020  1Password & Agilebits (@1Password)
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
